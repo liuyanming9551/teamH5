0d1d66d0a51478065b0304455cffee2257281f8f
liuyanming9551 <694918688@qq.com>
2019-02-21 19:01:38 +0800

    代码提交

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import "./common.less";
-import './rem.js'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
