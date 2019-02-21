8e1d435822fca538d1ef7f69c6e756fec09aad10
liuyanming9551 <694918688@qq.com>
2019-02-21 13:12:51 +0800

    代码修改

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,19 @@
-const { override, addLessLoader, fixBabelImports, addBabelPlugin, addBabelPlugins } = require('customize-cra');
+
+
+function overWrite(config){
+  //实现自定义配置
+  return config;
+}
+
+const { override, addLessLoader, fixBabelImports} = require('customize-cra');
 module.exports = override(
   addLessLoader({
     strictMath: true,
-    noIeCompat: true
+    noIeCompat: true,
+    javascriptEnabled: false
   }),
   fixBabelImports("import", {
     libraryName: "antd-mobile",
-    libraryDirectory: 'es',
     style: "css"
   })
 );
