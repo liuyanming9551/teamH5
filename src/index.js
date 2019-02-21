f1c334e637878dc554788d28ed4bd847166ed644
julvxiaoshuo <l>
2019-02-21 19:45:50 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import "./common.less";
+import './rem.js'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
